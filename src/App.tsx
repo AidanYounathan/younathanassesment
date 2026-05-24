4d4375d931bdabe425634803265ab2be76491d44
Aidan <ayounathan05@gmail.com>
2026-05-23 23:05:23 -0700

    [feat]: Scaffold top section

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,17 @@
 
 function App() {
   return (
-    <>
-    </>
+    <main className="min-h-screen px4 py-12">
+      <section className="mx-auto flex max-w-fl flex-col items-center">
+        <div>
+          <div className="flex flex-col items-center">
+            <h1 className="mt-6 text-3xl font-bold">Aidan Younathan</h1>
+            <p className="mt-2 text-base font-semibold sm:text-lg">Software Engineer Associate · DigitalNEST</p>
+            <p className="mt-1 flex items-center gap-1 text-sm "> 📍 Modesto, CA</p>
+          </div>
+        </div>
+      </section>
+    </main>
   )
 }
 
